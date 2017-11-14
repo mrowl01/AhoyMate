43a2ddf31f391776d078429a9c7ab77ba68c85a8
Chris <christopherortega56@gmail.com>
2017-11-13 23:32:28 -0600

    Experimenting with networking. Now able to communicate with other devices via LAN acting as a host and seeing each others movement

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; 
+
+public class MyHUD : NetworkManager 
+{
+	
+	public void MyStartHost()
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Starting host " ); 
+		StartHost ();
+	}
+
+	public override void OnStartHost ()
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Host started "); 
+	}
+	public override void OnStartClient(NetworkClient myClient)
+	{
+		Debug.Log ("Client start requested"); 
+		InvokeRepeating ("PrintDot", 0f, 1f);
+			
+	}
+	public override void OnClientConnect ( NetworkConnection conn)
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connect to IP: "+ conn.address); 
+		CancelInvoke ();
+	}
+	void PrintDot ()
+	{
+		Debug.Log (".");
+	}
+}
---
+++ Assets/Players/Player1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; 
+using UnityStandardAssets.CrossPlatformInput; 
+
+public class Player1 : NetworkBehaviour {
+
+	private Vector3 moving; 
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!isLocalPlayer) {
+			return;
+		} 
+			moving.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+			moving.y = 0;
+			moving.z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+			transform.Translate (moving); 
+
+		
+	}
+	 public override void OnStartLocalPlayer ()
+	{
+		GetComponentInChildren<Camera>().enabled= true; 
+	}
+}
